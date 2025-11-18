68fe17e5e2ecf3272443869ee9405e83fa8f0287
Ninad Agrawal <98337027+f20212948@users.noreply.github.com>
2025-11-18 16:15:52 +0530

    Add files via upload

---
+++ DAY6_17112025_NLP/straive_sentiment.py
@@ -0,0 +1,70 @@
+#Cleaning the text
+#1. create a text file and take the text from it
+#2. convert the letters into the lowercase
+#3. remove the punctuations like ,.!?
+import string
+from collections import Counter
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+
+# 1. read the text 
+text = open('read.txt',encoding='utf-8').read()
+print(text)
+
+# 2. convert to lower case
+lower_case = text.lower()
+print(lower_case)
+
+# remove punctuation
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+print(cleaned_text)
+
+#Tokenization --> splitting text into words
+tokenized_words = cleaned_text.split()
+print(tokenized_words)
+
+
+# removing stop words
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+## Final words = (al words) - (stop words)
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print(final_words)
+
+
+# Reading Emotions from the emotions.txt
+emotion_list = []
+with open('sentiment.txt', 'r') as file:
+    for line in file:
+        cleaned_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = cleaned_line.split(':')
+       # print("word :" + word + " " +"Emotion :" + emotion)
+        if word in final_words:
+            # print(f"{word} --> {emotion}")
+            emotion_list.append(emotion)
+print(emotion_list)
+w = Counter(emotion_list)
+print(w)
+
+
+# Plotting the emotions on the graph
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
+
+
